14822e5c19447ff1669aeffd83045d03bd95b621
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-03 18:13:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,9 +26,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
-  # If no arguments are passed, use default values:
-  # title == "Home Alone"
-  # release_date == 1990
+  #or can just do m.title = args[:title] and so on
   Movie.create do |m|
     args.each {|key, value| m.send("#{key}=", value)}
   end
@@ -44,18 +42,17 @@ end
 
 def can_get_size_of_the_database
   Movie.all.size
+  ## or Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
   Movie.find_by id: 1
+  #or can put parenthesis around the id: 1
 end
 
 def can_find_by_multiple_attributes
-  # Search Values:
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
   Movie.find_by title: "Title", release_date: 2000, director: "Me"
+  #can also put parenthesis around everything
 end
 
 def can_find_using_where_clause_and_be_sorted
@@ -66,9 +63,9 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  found = Movie.find_by title: "Awesome Flick"
+  found = Movie.create(title: "Awesome Flick")
   Movie.update(found.id, :title => "Even Awesomer Flick")
+  Movie.save
 end
 
 def can_update_using_update_method
